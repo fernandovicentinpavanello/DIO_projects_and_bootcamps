b765b6c7dee4aafcefe2abd9491e4209b59d26af
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-06-23 15:57:02 -0300

    Uso de variáveis tipo boolean e tipo string

--- Bootcamps & Courses/mentoTypescript/app.ts
+++ Bootcamps & Courses/mentoTypescript/app.ts
@@ -2,14 +2,21 @@ let button = document.getElementById('button');
 let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function adicionarNumeros(num1: number, num2: number) {
-    return num1 + num2
+function adicionarNumeros(num1: number, num2: number, printarResultado: boolean, frase: string) {
+    let resultado = num1 + num2;
+    if (printarResultado) {
+        console.log(frase + resultado)
+    }
+    return resultado
 }
+
+let printarResultado = true;
+let frase = 'O resultado é igual a: '
 
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            adicionarNumeros(Number(input1.value), Number(input2.value))
+            console.log(adicionarNumeros(Number(input1.value), Number(input2.value), printarResultado, frase));
         }
     })
 }
